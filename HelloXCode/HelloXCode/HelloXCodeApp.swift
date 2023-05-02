7d349a9c55d6187ca60f4ad93361ad5ae4b2ab64
Betico1928 <betoveluis@gmail.com>
2023-05-02 14:25:17 -0500

    Initial commit

---
+++ HelloXCode/HelloXCode/HelloXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXCodeApp.swift
+//  HelloXCode
+//
+//  Created by TARS 2.0 on 2/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
